3fb5edaa5e8dd0ac5613788fc775feba63d0e791
Diego Sait Grande Camacho <remumex16dansei@gmail.com>
2019-08-13 22:34:51 -0500

    Agregar archivos de proyecto.

---
+++ 04.DAL/DAL/DataAccess.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DAL
+{
+    public class DataAccess
+    {
+        #region Singleton
+        private static volatile DataAccess instance = null;
+        private static readonly object padlock = new object();
+        readonly string conString = "Data Source=DESKTOP-D2G24SC;Initial Catalog=CollectIT;User ID=test;Password=test;Persist Security Info=True;";
+
+
+        private DataAccess() { }
+        public static DataAccess Instance()
+        {
+            if (instance == null)
+                lock (padlock)
+                    if (instance == null)
+                        instance = new DataAccess();
+            return instance;
+        }
+        #endregion
+
+        #region queryExecute
+        public DataTable Query(String query)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand cmd = new SqlCommand(query, con) { CommandType = CommandType.StoredProcedure })
+            {
+                con.Open();
+                DataTable resultado = new DataTable();
+                resultado.Load(cmd.ExecuteReader());
+                return resultado;
+            }
+        }
+
+        public DataTable Query(String query, SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand cmd = new SqlCommand(query, con) { CommandType = CommandType.StoredProcedure })
+            {
+                con.Open();
+                DataTable resultado = new DataTable();
+                cmd.Parameters.AddRange(parameters);
+                resultado.Load(cmd.ExecuteReader());
+                return resultado;
+            }
+        }
+
+        public int Execute(String query, SqlParameter[] parameters)
+        {
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand cmd = new SqlCommand(query, con) { CommandType = CommandType.StoredProcedure })
+            {
+                con.Open();
+                cmd.Parameters.AddRange(parameters);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        #endregion
+    }
+}
